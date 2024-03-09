2df69174818d0a7c63f62b0ef05cf3d1c9833235
TealSmurf <jcoin707@gmail.com>
2024-03-08 23:02:31 -0800

    Added displays

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
-#include    <iostream>
+#include  <iostream>
 using namespace std;
 
 int main()
 {
    
    int M;
-   "Enter total number of rows = columns: "
-   cout >> M;
+   cout << "Enter total number of rows = columns: ";
+   cin >> M;
 
    for(int i = 0; i < M; i++)
    {
-      for(int j = 0; j < M; i++)
+      for(int j = 0; j < M; j++)
       {
-         
+         cout << i << " " << j << endl;
       }
    }
   
