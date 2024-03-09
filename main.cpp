4527e3d482f3299174e3c95ffb872074d2126439
TealSmurf <jcoin707@gmail.com>
2024-03-08 22:55:25 -0800

    Initial structure of program

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 int main()
 {
    
-   /* Complete your code */
+   int M;
+   "Enter total number of rows = columns: "
+   cout >> M;
+
+   for(int i = 0; i < M; i++)
+   {
+      for(int j = 0; j < M; i++)
+      {
+         
+      }
+   }
   
 }
